51a257a467fb67643bf489e30cd69770468c59ae
Ayamba Sumaila <sumailaayamba15@gmail.com>
2025-02-01 21:49:13 +0000

    "Cellphone class added"

--- Cellphone.java
+++ Cellphone.java
@@ -1,2 +1,34 @@
+/*
+*
+* for the CellPhone class
+CellPhone
+- manufact : String
+- model : String
+- retailPrice : double
++ CellPhone(man : String, mod : String,
+price : double);
++ setManufact(man : String) : void
++ setModel(mod : String) : void
++ setRetailPrice(price : double) : void
++ getManufact() : String
++ getModel() : String
++ getRetailPrice() : double
+*
+*
+*
+* */
+
+
 public class Cellphone {
+
+    private String manufact, model;
+    private double retailPrice;
+
+    public Cellphone(String man, String mod, double price)
+    {
+        manufact=man;
+        model=mod;
+        retailPrice=price;
+
+    }
 }
